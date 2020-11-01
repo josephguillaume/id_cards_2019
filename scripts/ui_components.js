59283feff962b39818b0aa41c824ac91181390ef
Joseph Guillaume <josephguillaume@gmail.com>
2020-11-01 19:57:20 +1100

    Define page in rdf instead

---
+++ scripts/ui_components.js
@@ -0,0 +1,91 @@
+const VOC = new $rdf.Namespace("http://example.com/vocab/");
+
+class Page {
+  constructor(page_content) {
+    this.registeredComponents = {};
+    this.store = $rdf.graph();
+    this.page_uri = $rdf.sym("http://example.com/conceptualisation"); // TODO
+    this.page_object = $rdf.sym("http://example.com/conceptualisation#this"); // TODO
+    $rdf.parse(page_content, this.store, this.page_uri.value);
+    this.title = this.store.any(this.page_object, VOC("title"), null).value;
+    this.target_subject = this.store.any(
+      this.page_object,
+      VOC("target_subject"),
+      null
+    ).value;
+  }
+  registerComponent(namedNode, classRef) {
+    this.registeredComponents[namedNode.value] = classRef;
+  }
+}
+
+class PredicateUL {
+  constructor(page, subject, predicate) {
+    this.wanted_statements = page.statements.filter(
+      s => (s.subject == subject) & (s.predicate == predicate)
+    );
+    this.subject = subject;
+    this.predicate = predicate;
+  }
+  render() {
+    if (this.wanted_statements.length == 0) return "\n";
+    var innerHTML = `\n\n<li><span class=predicate>${this.predicate}</span>\n<ul>\n\n`;
+    this.wanted_statements.forEach(
+      s =>
+        (innerHTML += `<li title='${s.documentName}: ${s.text}'><a href='${s.link}' target='_new'>${s.object}</a></li>`)
+    );
+    innerHTML += "</ul></li>\n";
+    return innerHTML;
+  }
+}
+class HTMLANDFILTERSUBGROUP {
+  constructor(page, subject, predicate) {
+    this.subgroups = page.statements.filter(
+      s => (s.subject == subject) & (s.predicate == predicate)
+    );
+    this.subject = subject;
+    this.predicate = predicate;
+    this.blocks = this.subgroups.map(s => new SubjectBlock(page, s.object));
+  }
+  render() {
+    if (this.subgroups.length == 0) return "\n";
+    var innerHTML = `\n\n<li><span class=predicate>${this.predicate}</span>\n<ul>\n\n`;
+    this.subgroups.forEach(
+      (s, i) =>
+        (innerHTML +=
+          `<li><span title='${s.documentName}: ${s.text}'><a href='${s.link}' target='_new'>${s.object}</a></li>` +
+          this.blocks[i].render()) + "</li>\n"
+    );
+    innerHTML += "</ul></li>\n";
+    return innerHTML;
+  }
+}
+
+class SubjectBlock {
+  constructor(page, subject) {
+    this.page = page;
+    this.subject = subject.replace(/.*#/, ""); //TODO
+    const predicates = page.store.match(null, VOC("predicates"), null)[0].object
+      .elements;
+    this.blocks = predicates.map(p => this.handlePredicate(p));
+    this.nstatements = page.statements.filter(
+      x =>
+        x.subject == this.subject &&
+        predicates.map(x => x.value).indexOf(x.predicate) > -1
+    ).length;
+  }
+  handlePredicate(p) {
+    let component = this.page.store.any(p, VOC("defaultDisplayComponent"), null)
+      .value;
+    let componentClass = this.page.registeredComponents[component];
+    //TODO: get label for each predicate
+    return new componentClass(this.page, this.subject, p.value);
+  }
+  render() {
+    if (this.nstatements == 0) return "\n";
+    var innerHTML = "<div class=subject_block><ul>\n";
+    this.blocks.forEach(b => (innerHTML += b.render()));
+    innerHTML += "</ul></div>\n";
+    return innerHTML;
+  }
+}
